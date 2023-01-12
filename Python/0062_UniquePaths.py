6abf5bf6d112af3d83f095266b05299fe946abab
Joshua Valdez <josh@augnos.com>
2023-01-11 17:32:55 -0800

    Completed 0062 Unique Paths

---
+++ Python/0062_UniquePaths.py
@@ -0,0 +1,19 @@
+# There is a robot on an m x n grid. 
+# The robot is initially located at the top-left corner (i.e., grid[0][0]). 
+# The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). 
+# The robot can only move either down or right at any point in time.
+
+# Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+# The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+import math
+class Solution(object):
+    def uniquePaths(self, m, n):
+        return (math.factorial(m+n-2)/(math.factorial(m-1) * math.factorial(n-1)))
+
+test = Solution()
+
+print(test.uniquePaths(3,7))
+print(test.uniquePaths(3,2))
+print(test.uniquePaths(99,2))
